3f669dea2912db6d04ef7322cf5de9d8b9b775ea
Vasanth Kumar <itsparser@gmail.com>
2020-06-04 02:05:21 +0530

    resolve ccommit

--- main.go
+++ main.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"github.com/airstrik/airstrik/src/route"
 	"github.com/airstrik/gobase/pkg/server"
-=======
-	"encoding/json"
-	"fmt"
-	"github.com/airstrik/airstrik/pkg/core/model/engine"
-	engine2 "github.com/airstrik/airstrik/pkg/engine"
-	"github.com/tebeka/selenium"
-	"io/ioutil"
-	"log"
->>>>>>> 2a9a8f22cfa94fe637f4f98462e1c3f5603af100
 	"os"
 )
 var (
